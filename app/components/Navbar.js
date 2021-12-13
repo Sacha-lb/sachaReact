250df503ca82339585c98780e92d72c3c11db589
Slb <sacha.le_bras@edu.devinci.fr>
2021-12-13 14:44:24 +0100

    Add navbar

---
+++ app/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from "react";
+import styles from '../styles/Navbar.module.css';
+
+export default function Navbar(){
+    const [user, setUser] = useState(null);
+    return (
+        <div className={styles.navcontainer}>
+            <h3>NFT Marketplace</h3>
+            <p>Menu</p>
+            <div>
+                { user ? (
+                    <button>Logout</button>
+                ) : (
+                    <button>Login</button>
+                )}
+            </div>
+        </div>
+    )
+}
