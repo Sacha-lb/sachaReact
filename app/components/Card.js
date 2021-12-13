9ab358cd59a300fb55da44f10dc4ba44a729d2e2
Slb <sacha.le_bras@edu.devinci.fr>
2021-12-13 16:08:38 +0100

    Add css cards

--- app/components/Card.js
+++ app/components/Card.js
@@ -6,10 +6,11 @@ export default function Marketplace(props){
     const [user, setUser] = useState(null);
     return (
         <div className={styles.cardcontainer}>
-            <p>{props.title}</p>
-            <Image width="100%" height="100%" src={props.img} alt="" />
+            <p className={styles.cardtitle}>{props.title}</p>
+            <Image className={styles.cardimg} width="100%" height="100%" src={props.img} alt=""/>
             <p>{props.desc}</p>
             <p>{props.price} euros</p>
+            <button className={styles.cardbtn}>BUY</button>
         </div>
     )
 }
